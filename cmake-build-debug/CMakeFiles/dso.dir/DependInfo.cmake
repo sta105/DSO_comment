09825269f144780fd91272660b6c4b83f87b220f
Sicong Tang <sta105@sfu.ca>
2017-06-16 19:28:46 -0700

    first commit

---
+++ cmake-build-debug/CMakeFiles/dso.dir/DependInfo.cmake
@@ -0,0 +1,55 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/sicong/dso/src/FullSystem/CoarseInitializer.cpp" "/home/sicong/dso/cmake-build-debug/CMakeFiles/dso.dir/src/FullSystem/CoarseInitializer.cpp.o"
+  "/home/sicong/dso/src/FullSystem/CoarseTracker.cpp" "/home/sicong/dso/cmake-build-debug/CMakeFiles/dso.dir/src/FullSystem/CoarseTracker.cpp.o"
+  "/home/sicong/dso/src/FullSystem/FullSystem.cpp" "/home/sicong/dso/cmake-build-debug/CMakeFiles/dso.dir/src/FullSystem/FullSystem.cpp.o"
+  "/home/sicong/dso/src/FullSystem/FullSystemDebugStuff.cpp" "/home/sicong/dso/cmake-build-debug/CMakeFiles/dso.dir/src/FullSystem/FullSystemDebugStuff.cpp.o"
+  "/home/sicong/dso/src/FullSystem/FullSystemMarginalize.cpp" "/home/sicong/dso/cmake-build-debug/CMakeFiles/dso.dir/src/FullSystem/FullSystemMarginalize.cpp.o"
+  "/home/sicong/dso/src/FullSystem/FullSystemOptPoint.cpp" "/home/sicong/dso/cmake-build-debug/CMakeFiles/dso.dir/src/FullSystem/FullSystemOptPoint.cpp.o"
+  "/home/sicong/dso/src/FullSystem/FullSystemOptimize.cpp" "/home/sicong/dso/cmake-build-debug/CMakeFiles/dso.dir/src/FullSystem/FullSystemOptimize.cpp.o"
+  "/home/sicong/dso/src/FullSystem/HessianBlocks.cpp" "/home/sicong/dso/cmake-build-debug/CMakeFiles/dso.dir/src/FullSystem/HessianBlocks.cpp.o"
+  "/home/sicong/dso/src/FullSystem/ImmaturePoint.cpp" "/home/sicong/dso/cmake-build-debug/CMakeFiles/dso.dir/src/FullSystem/ImmaturePoint.cpp.o"
+  "/home/sicong/dso/src/FullSystem/PixelSelector2.cpp" "/home/sicong/dso/cmake-build-debug/CMakeFiles/dso.dir/src/FullSystem/PixelSelector2.cpp.o"
+  "/home/sicong/dso/src/FullSystem/Residuals.cpp" "/home/sicong/dso/cmake-build-debug/CMakeFiles/dso.dir/src/FullSystem/Residuals.cpp.o"
+  "/home/sicong/dso/src/IOWrapper/OpenCV/ImageDisplay_OpenCV.cpp" "/home/sicong/dso/cmake-build-debug/CMakeFiles/dso.dir/src/IOWrapper/OpenCV/ImageDisplay_OpenCV.cpp.o"
+  "/home/sicong/dso/src/IOWrapper/OpenCV/ImageRW_OpenCV.cpp" "/home/sicong/dso/cmake-build-debug/CMakeFiles/dso.dir/src/IOWrapper/OpenCV/ImageRW_OpenCV.cpp.o"
+  "/home/sicong/dso/src/IOWrapper/Pangolin/KeyFrameDisplay.cpp" "/home/sicong/dso/cmake-build-debug/CMakeFiles/dso.dir/src/IOWrapper/Pangolin/KeyFrameDisplay.cpp.o"
+  "/home/sicong/dso/src/IOWrapper/Pangolin/PangolinDSOViewer.cpp" "/home/sicong/dso/cmake-build-debug/CMakeFiles/dso.dir/src/IOWrapper/Pangolin/PangolinDSOViewer.cpp.o"
+  "/home/sicong/dso/src/OptimizationBackend/AccumulatedSCHessian.cpp" "/home/sicong/dso/cmake-build-debug/CMakeFiles/dso.dir/src/OptimizationBackend/AccumulatedSCHessian.cpp.o"
+  "/home/sicong/dso/src/OptimizationBackend/AccumulatedTopHessian.cpp" "/home/sicong/dso/cmake-build-debug/CMakeFiles/dso.dir/src/OptimizationBackend/AccumulatedTopHessian.cpp.o"
+  "/home/sicong/dso/src/OptimizationBackend/EnergyFunctional.cpp" "/home/sicong/dso/cmake-build-debug/CMakeFiles/dso.dir/src/OptimizationBackend/EnergyFunctional.cpp.o"
+  "/home/sicong/dso/src/OptimizationBackend/EnergyFunctionalStructs.cpp" "/home/sicong/dso/cmake-build-debug/CMakeFiles/dso.dir/src/OptimizationBackend/EnergyFunctionalStructs.cpp.o"
+  "/home/sicong/dso/src/util/Undistort.cpp" "/home/sicong/dso/cmake-build-debug/CMakeFiles/dso.dir/src/util/Undistort.cpp.o"
+  "/home/sicong/dso/src/util/globalCalib.cpp" "/home/sicong/dso/cmake-build-debug/CMakeFiles/dso.dir/src/util/globalCalib.cpp.o"
+  "/home/sicong/dso/src/util/settings.cpp" "/home/sicong/dso/cmake-build-debug/CMakeFiles/dso.dir/src/util/settings.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "ENABLE_SSE"
+  "HAS_ZIPLIB=1"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/opencv"
+  "../src"
+  "../thirdparty/Sophus"
+  "../thirdparty/sse2neon"
+  "/usr/include/eigen3"
+  "/usr/local/lib/cmake/Pangolin/../../../include"
+  "/usr/local/include"
+  "/usr/include/suitesparse"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/dso_dataset.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/sicong/dso/src/main_dso_pangolin.cpp" "/home/sicong/dso/cmake-build-debug/CMakeFiles/dso_dataset.dir/src/main_dso_pangolin.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "ENABLE_SSE"
+  "HAS_ZIPLIB=1"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/opencv"
+  "../src"
+  "../thirdparty/Sophus"
+  "../thirdparty/sse2neon"
+  "/usr/include/eigen3"
+  "/usr/local/lib/cmake/Pangolin/../../../include"
+  "/usr/local/include"
+  "/usr/include/suitesparse"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/sicong/dso/cmake-build-debug/CMakeFiles/dso.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
